f9e4bae3713aadb8dfdb1b2bfbd9ed0f42b77fe9
Aryan Kumar <aryankumar4848@outlook.com>
2026-03-17 02:46:33 +0530

    Updates

--- _next/static/8VZmj1-IP-VgSLq-Dc9G4/_buildManifest.js
+++ _next/static/8VZmj1-IP-VgSLq-Dc9G4/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/developer-portfolio//_next/:path+",
-        "destination": "/developer-portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
